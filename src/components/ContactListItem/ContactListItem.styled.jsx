cb43c5fc61ab14e502c10c4beda8613ba08d992f
petekostrytsya <petekostrytsya@gmail.com>
2023-05-25 23:11:43 +0300

    add ContactListItem

---
+++ src/components/ContactListItem/ContactListItem.styled.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+
+export const ContactItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ContactName = styled.p`
+  font-family: Segoe UI;
+  font-weight: 500;
+  margin: 0;
+  font-weight: 500;
+`;
+
+export const ContactNumber = styled.span`
+  margin-left: 10px;
+`;
+
+export const Button = styled.button`
+  margin: 8px;
+  padding: 4px 14px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
